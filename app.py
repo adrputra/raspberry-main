1e6c86d052d2f11e1e7e0526de129231a1bc73ac
Andra Putra <ricardoandra17@gmail.com>
2025-03-11 13:22:43 +0700

    rollback

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask_socketio import SocketIO
 from src.config.config import config
 from src.route.route import routes
 from src.nfc.attendance import attendance
-import logging
-import time
 
 app = Flask(__name__)
 CORS(app)
@@ -19,20 +17,10 @@ def nfc_status():
     return render_template('nfc_status.html')
 
 def start_nfc_scanner():
-    while True:
-        try:
-            socketio.emit('nfc_status', {'message': 'NFC scanner started'})
-            print("Starting NFC scanner...")
-            attendance(socketio)
-        except Exception as e:
-            logging.error(f"Error in NFC scanner: {e}")
-            time.sleep(5)  # Wait before restarting the scanner
+    socketio.emit('nfc_status', {'message': 'NFC scanner started'})
+    print("Starting NFC scanner...")
+    attendance(socketio)
 
 if __name__ == '__main__':
-    while True:
-        try:
-            socketio.start_background_task(start_nfc_scanner)
-            socketio.run(app, host=config["server"]["host"], port=config["server"]["port"], debug=config["server"]["debug"])
-        except Exception as e:
-            logging.error(f"Error starting the application: {e}")
-            time.sleep(5)  # Wait before restarting the application
+    socketio.start_background_task(start_nfc_scanner)
+    socketio.run(app, host=config["server"]["host"], port=config["server"]["port"], debug=config["server"]["debug"])
